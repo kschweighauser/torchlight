a21bd4262ac0189a3a87391a95fa62728a0a6fca
kschweighauser <kms@extanto.com>
2020-01-13 09:32:24 -0800

    Adding a torchlight favicon.ico to generic setup.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Nav from './components/Nav'
 import Header from './components/Header'
 import About from './components/About'
-import Shop from './components/Shop'
+import Events from './components/Events'
 import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
+    <div>
+      <Nav />
+      <Header />
+      <About />
+      <Events />
+      <Footer />
     </div>
   );
 }

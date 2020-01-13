a0555676840c6c7e6ae584c6869db0f3a9c073f6
kschweighauser <kms@extanto.com>
2020-01-13 09:07:55 -0800

    Adding basic site navigation and routes to the react project.

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function Footer() {
+  return (
+    <nav>
+      <h3>Footer Placeholder</h3>
+      <ul>
+          <li>Personal LinkedIn</li>
+          <li>Personal Twitter</li>
+          <li>Extanto Site</li>
+          <li>Extanto Twitter</li>
+          <li>Deove Site</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <nav>
+      <h3>Header Placeholder</h3>
+      <ul>
+          <li>Some Item</li>
+          <li>Some Other Item</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
